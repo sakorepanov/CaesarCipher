1daf12aa3bec95f45c2fba072d5d0feced67090a
Sergey Korepanov <s.a.korepanov@mail.ru>
2019-12-30 15:39:17 +0400

    Расшифровка шифра Цезаря

---
+++ src/CaesarCypher.java
@@ -0,0 +1,27 @@
+public class CaesarCypher {
+    public static void main(String[] args) {
+        String input = "Еъёчхф Вхзёюлх, адздёиу ф ждэщхб, црбх еёдюэчъщъгюъв южаижжзчх, ждчъёнъгжзчдв. Ъы зёюивй жёхчгюв бюнс ж ъы вдгивъгзхбсгрв аёхкдв. \n" +
+                "Зъеъёс вгъ дмъчющгд, мзд гъюэцъьгджзс тздшд аёхкх фчбфъзжф жбъщжзчюъв гъждчъёнъгжзчх мъбдчъмъжадшд югщючющиивх. \n" +
+                "Ф юэимюб чхни южздёюу ю чгыж юэвъгъгюф, здмгъъ дзёхэюч еджздфггиу юэвъгмючджзс мъбдчъмъжаюк едёдадч. \n" +
+                "Ю зъв гъ въгъъ, еджбъщдчхбх гъищхмх. Ф еёюнъб а чрчдщи, мзд чюгдя чжъви вдя югзъббъаз, х чдэвдьгд, вды мёъэвъёгдъ жзёъвбъгюъ ад чжъви шхёвдгюмгдви. \n" +
+                "Гхязю ёънъгюъ вгъ едвдшбх еёдшёхввх югзиюзючгдшд зюех, жеълюхбсгд ждэщхггхф щбф юэимъгюф деёъщъбъггрк жздёдг мъбдчъмъжадя щиню. \n" +
+                "Въгф вдьгд гхэчхзс дзлдв Вхзёюлр, х ъы, цъэ еёъичъбюмъгюф, вхзъёсу.";
+        String alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        char[] inputArray = input.toCharArray();
+        for (int j = 1; j < 33; j++) {
+            for (int i = 0; i < inputArray.length; i++) {
+                if (Character.isLetter(inputArray[i])) {
+                    if (Character.isUpperCase(inputArray[i])) {
+                        int index = alphabet.indexOf(Character.toLowerCase(input.charAt(i)));
+                        inputArray[i] = Character.toUpperCase(alphabet.charAt(index + j));
+                    } else {
+                        int index = alphabet.indexOf(input.charAt(i));
+                        inputArray[i] = alphabet.charAt(index + j);
+                    }
+                }
+                System.out.print(inputArray[i]);
+            }
+            System.out.println("\n");
+        }
+    }
+}
